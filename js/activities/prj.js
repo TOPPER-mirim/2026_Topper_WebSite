7ea25bc22cab6d569d050c901d48d228cb8d709d
오세영 <leenight5309@naver.com>
2026-02-22 04:09:43 +0900

    feat: 수상 경력 추가

---
+++ js/activities/prj.js
@@ -0,0 +1,22 @@
+fetch("../json/activities/project.json")
+  .then((res) => res.json())
+  .then((data) => {
+    const container = document.getElementById("prjs");
+
+    data.forEach((item) => {
+      const card = document.createElement("div");
+      card.className = "project_card";
+
+      card.innerHTML = `
+        <div class="prj_img">
+          <img src="../img/${item.img}" alt="${item.name}">
+        </div>
+        <div class="project_info">
+          <p class="prj_name">${item.name}</p>
+          <p class="prj_ex">${item.ex}</p>
+        </div>
+      `;
+
+      container.appendChild(card);
+    });
+  });
---
+++ js/activities/profile.js
@@ -0,0 +1,40 @@
+function loadAll() {
+  fetch('../json/activities/profile/index.json')
+    .then(res => res.json())
+    .then(list => {
+      const peopleBox = document.getElementById('people_list');
+      const awardBox = document.getElementById('award_container');
+
+      let peopleHTML = '';
+      let awardHTML = '';
+
+      for (let i = 0; i < list.length; i++) {
+        const person = list[i];
+
+        fetch(person.path)
+          .then(res => res.json())
+          .then(awards => {
+
+            let groupHTML = `
+              <div class="p_div">
+                <div class="p_name">${person.name}</div>
+            `;
+
+            for (let j = 0; j < awards.length; j++) {
+              groupHTML += `
+                <div class="p_award">${awards[j].name}</div>
+              `;
+            }
+
+            groupHTML += `</div>`;
+
+            awardHTML += groupHTML;
+            awardBox.innerHTML = awardHTML;
+          });
+      }
+
+      peopleBox.innerHTML = peopleHTML;
+    });
+}
+
+loadAll();
