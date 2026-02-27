10558c3b9e70c99e4f83eceb57058cb94275dff1
오세영 <leenight5309@naver.com>
2026-02-27 23:11:23 +0900

    feat: 햄버거 완성

--- js/main/hamburger.js
+++ js/main/hamburger.js
@@ -1,8 +1,15 @@
 const hamburger = document.getElementById("hamburger");
 const sideMenu = document.getElementById("sideMenu");
+const back = document.getElementById("back");
 
-hamburger.addEventListener("click", () => {
+hamburger.addEventListener("click", (e) => {
+  e.stopPropagation();
   sideMenu.classList.toggle("active");
+});
+
+back.addEventListener("click", (e) => {
+  e.stopPropagation();
+  sideMenu.classList.remove("active");
 });
 
 document.addEventListener("click", (e) => {
